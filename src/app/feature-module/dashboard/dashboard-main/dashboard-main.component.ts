67cf1d8a73be28a04d816d81adc60273e419fd04
ahmed hassan <a7med.abdo6@gmail.com>
2024-02-17 14:35:57 +0200

    translte school

--- src/app/feature-module/dashboard/dashboard-main/dashboard-main.component.ts
+++ src/app/feature-module/dashboard/dashboard-main/dashboard-main.component.ts
@@ -18,6 +18,8 @@ import {
   ApexResponsive,
 } from 'ng-apexcharts';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { DashboardService } from '../dashboard.service';
+import { TranslateService } from '@ngx-translate/core';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries | any;
@@ -43,15 +45,18 @@ export type ChartOptions = {
 })
 export class DashboardMainComponent {
   @ViewChild('chart') chart!: ChartComponent;
+  schoolWithZoneAndCurriculumFilter: any;
+  schoolsZone: any;
+  schoolsCurriculum: any;
 
-  public chartOptionsOne: Partial<ChartOptions>;
+  public chartOptionsOne: any;
 
   colors = ['#1D4BA3', '#FF624D', '#42B4E3', '#FFAA31', '#1DA39B'];
-  public chartOptionsTwo: Partial<ChartOptions>;
+  public chartOptionsTwo: any;
   public chartOptionsTotalBranches: Partial<ChartOptions>;
   public chartOptionsTotalBranchesCompleted: Partial<ChartOptions>;
   public chartOptionsTotalBranchesUncompleted: Partial<ChartOptions>;
-
+  currentLang: any;
   public chartOptionsTotalBranchesEmpty: Partial<ChartOptions>;
 
   public chartOptionsMultiRadial: Partial<ChartOptions>;
@@ -101,285 +106,12 @@ title="fa fa-chevron-left"
     },
     nav: true,
   };
-  constructor() {
-    this.chartOptionsOne = {
-      series: [
-        {
-          name: 'Net Profit',
-          data: [44, 55, 57, 56, 61, 58],
-        },
-      ],
-      fill: {
-        type: 'solid',
-        colors: [
-          '#1AA8DF',
-          '#FFB54B',
-          '#0E8A74',
-          '#FF624D',
-          '#3E4954',
-          '#92BE33',
-          '#42B4E3',
-        ],
-      },
-      chart: {
-        type: 'bar',
-        width: 500,
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false,
-          distributed: true,
-          borderRadius: 20,
-          dataLabels: {
-            show: false,
-            position: 'top',
-          },
-        },
-      },
+  constructor(
+    private dashboardService: DashboardService,
+    public translate: TranslateService
+  ) {
+    this.currentLang = this.translate.currentLang;
 
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        show: true,
-        width: 2,
-        colors: ['transparent'],
-      },
-      xaxis: {
-        labels: {
-          style: {
-            colors: ['#000', '#000', '#000', '#000', '#000', '#000', '#000'],
-            fontSize: '12px',
-          },
-        },
-        categories: ['إماراتي', 'مصري', 'قطرى', 'كويتى', 'سعودى', 'مغربى'],
-      },
-      yaxis: {
-        labels: {
-          style: {
-            colors: ['#000', '#000', '#000', '#000', '#000', '#000', '#000'],
-            fontSize: '12px',
-          },
-        },
-      },
-      // fill: {
-      //   opacity: 1,
-      // },
-      grid: {
-        show: true, // you can either change hear to disable all grids
-        xaxis: {
-          lines: {
-            show: false, //or just here to disable only x axis grids
-          },
-        },
-        yaxis: {
-          lines: {
-            show: true, //or just here to disable only y axis
-          },
-        },
-      },
-      tooltip: {
-        y: {
-          formatter: function (val: string) {
-            return '$ ' + val + ' thousands';
-          },
-        },
-      },
-      responsive: [
-        {
-          breakpoint: 1100,
-          options: {
-            chart: {
-              width: 450,
-              // height: 350,
-            },
-          },
-        },
-        {
-          breakpoint: 500,
-          options: {
-            chart: {
-              width: 380,
-              // height: 350,
-            },
-          },
-        },
-        {
-          breakpoint: 400,
-          options: {
-            chart: {
-              width: 280,
-              // height: 350,
-            },
-            plotOptions: {
-              bar: {
-                horizontal: false,
-                distributed: true,
-                borderRadius: 10,
-                dataLabels: {
-                  show: false,
-                  position: 'top',
-                },
-              },
-            },
-          },
-        },
-      ],
-    };
-
-    this.chartOptionsTwo = {
-      series: [
-        {
-          name: 'basic',
-          data: [400, 430, 448, 470, 540, 580],
-        },
-      ],
-      fill: {
-        type: 'solid',
-        colors: [
-          '#1AA8DF',
-          '#FFB54B',
-          '#0E8A74',
-          '#FF624D',
-          '#3E4954',
-          '#92BE33',
-          '#42B4E3',
-        ],
-      },
-      chart: {
-        type: 'bar',
-        // height: 350,
-        width: 430,
-      },
-      grid: {
-        show: false, // you can either change hear to disable all grids
-        xaxis: {
-          lines: {
-            show: false, //or just here to disable only x axis grids
-          },
-        },
-        yaxis: {
-          lines: {
-            show: false, //or just here to disable only y axis
-          },
-        },
-      },
-      responsive: [
-        {
-          breakpoint: 1360,
-          options: {
-            chart: {
-              width: 350,
-              // height: 350,
-            },
-          },
-        },
-        {
-          breakpoint: 1100,
-          options: {
-            chart: {
-              width: 450,
-              // height: 350,
-            },
-          },
-        },
-        {
-          breakpoint: 500,
-          options: {
-            chart: {
-              width: 350,
-              // height: 350,
-            },
-          },
-        },
-        {
-          breakpoint: 400,
-          options: {
-            chart: {
-              width: 280,
-              // height: 350,
-            },
-            plotOptions: {
-              bar: {
-                horizontal: false,
-                distributed: true,
-                borderRadius: 10,
-                dataLabels: {
-                  show: false,
-                  position: 'top',
-                },
-              },
-            },
-          },
-        },
-      ],
-      plotOptions: {
-        bar: {
-          horizontal: true,
-          distributed: true,
-          borderRadius: 10,
-          dataLabels: {
-            position: 'top',
-          },
-        },
-        dataLabels: {
-          enabled: false,
-          style: {
-            colors: ['#333'],
-          },
-          offsetX: 20,
-        },
-      },
-
-      legend: {
-        show: false,
-        position: 'top',
-        horizontalAlign: 'left',
-        floating: true,
-        offsetY: -10,
-        offsetX: -5,
-      },
-      dataLabels: {
-        enabled: true,
-        textAnchor: 'center',
-        style: {
-          colors: ['#fff'],
-        },
-
-        formatter: function (val: any, opt: any) {
-          return opt.w.globals.labels[opt.dataPointIndex] + ':  ' + val;
-        },
-        offsetX: 0,
-      },
-      yaxis: {
-        show: false,
-        reversed: true,
-      },
-      tooltip: {
-        theme: 'dark',
-        x: {
-          show: false,
-        },
-        y: {
-          show: false,
-        },
-      },
-      xaxis: {
-        show: false,
-
-        labels: {
-          show: false,
-        },
-        categories: [
-          'مدينة الشارقة',
-          'المنطقة الوسطى ',
-          'المنطقة الشرقية ',
-          'المجاز ',
-          'المنخول ',
-          'النهضة ',
-        ],
-      },
-    };
     this.chartOptionsTotalBranches = {
       series: [44],
       chart: {
@@ -639,23 +371,340 @@ title="fa fa-chevron-left"
       labels: ['Apples', 'Oranges'],
     };
   }
-  ngAfterViewInit(): void {
-    // Wait for ng-apexcharts to initialize
-    setTimeout(() => {
-      const apexChart: any = this.chart.chart;
-
-      if (apexChart && apexChart.w) {
-        const segments = apexChart.w.globals.dom.baseEl.querySelectorAll(
-          '.apexcharts-donut-series path'
+  ngOnInit() {
+    this.dashboardService
+      .getSchoolStatics({ curriculumIds: null })
+      .subscribe((res) => {
+        console.log(res);
+        const parsedLabels = res.schoolsCurriculum.labels.map((label: any) =>
+          JSON.parse(label)
         );
+        const parsedLabelsschoolWithZoneAndCurriculumFilter =
+          res.schoolWithZoneAndCurriculumFilter.labels.map((label: any) =>
+            JSON.parse(label)
+          );
 
-        segments.forEach((segment: any) => {
-          segment.setAttribute('rx', '10'); // Set border radius for x-axis
-          segment.setAttribute('ry', '10'); // Set border radius for y-axis
-        });
-      }
-    });
+        this.schoolsCurriculum = {
+          labels: parsedLabels,
+          series: res.schoolsCurriculum.series,
+        };
+        this.schoolsZone = res.schoolsZone;
+        this.schoolWithZoneAndCurriculumFilter = {
+          labels: parsedLabelsschoolWithZoneAndCurriculumFilter,
+          series: res.schoolWithZoneAndCurriculumFilter.series,
+        };
+
+        this.chartOptionsOne = {
+          series: [
+            {
+              name: 'Net Profit',
+              data: this.schoolsCurriculum?.series,
+            },
+          ],
+          fill: {
+            type: 'solid',
+            colors: [
+              '#1AA8DF',
+              '#FFB54B',
+              '#0E8A74',
+              '#FF624D',
+              '#3E4954',
+              '#92BE33',
+              '#42B4E3',
+            ],
+          },
+          chart: {
+            type: 'bar',
+            width: 500,
+          },
+          plotOptions: {
+            bar: {
+              horizontal: false,
+              distributed: true,
+              borderRadius: 20,
+              dataLabels: {
+                show: false,
+                position: 'top',
+              },
+            },
+          },
+
+          dataLabels: {
+            enabled: false,
+          },
+          stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent'],
+          },
+          xaxis: {
+            labels: {
+              style: {
+                colors: [
+                  '#000',
+                  '#000',
+                  '#000',
+                  '#000',
+                  '#000',
+                  '#000',
+                  '#000',
+                ],
+                fontSize: '12px',
+              },
+            },
+            categories: this.schoolsCurriculum?.labels.map((label: any) =>
+              this.currentLang == 'en' ? label.En : label.Ar
+            ),
+          },
+          yaxis: {
+            labels: {
+              style: {
+                colors: [
+                  '#000',
+                  '#000',
+                  '#000',
+                  '#000',
+                  '#000',
+                  '#000',
+                  '#000',
+                ],
+                fontSize: '12px',
+              },
+            },
+          },
+          // fill: {
+          //   opacity: 1,
+          // },
+          grid: {
+            show: true, // you can either change hear to disable all grids
+            xaxis: {
+              lines: {
+                show: false, //or just here to disable only x axis grids
+              },
+            },
+            yaxis: {
+              lines: {
+                show: true, //or just here to disable only y axis
+              },
+            },
+          },
+          tooltip: {
+            y: {
+              formatter: function (val: string) {
+                return '$ ' + val + ' thousands';
+              },
+            },
+          },
+          responsive: [
+            {
+              breakpoint: 1100,
+              options: {
+                chart: {
+                  width: 450,
+                  // height: 350,
+                },
+              },
+            },
+            {
+              breakpoint: 500,
+              options: {
+                chart: {
+                  width: 380,
+                  // height: 350,
+                },
+              },
+            },
+            {
+              breakpoint: 400,
+              options: {
+                chart: {
+                  width: 280,
+                  // height: 350,
+                },
+                plotOptions: {
+                  bar: {
+                    horizontal: false,
+                    distributed: true,
+                    borderRadius: 10,
+                    dataLabels: {
+                      show: false,
+                      position: 'top',
+                    },
+                  },
+                },
+              },
+            },
+          ],
+        };
+
+        this.chartOptionsTwo = {
+          series: [
+            {
+              name: 'basic',
+              data: this.schoolWithZoneAndCurriculumFilter.series,
+            },
+          ],
+          fill: {
+            type: 'solid',
+            colors: [
+              '#1AA8DF',
+              '#FFB54B',
+              '#0E8A74',
+              '#FF624D',
+              '#3E4954',
+              '#92BE33',
+              '#42B4E3',
+            ],
+          },
+          chart: {
+            type: 'bar',
+            // height: 350,
+            width: 430,
+          },
+          grid: {
+            show: false, // you can either change hear to disable all grids
+            xaxis: {
+              lines: {
+                show: false, //or just here to disable only x axis grids
+              },
+            },
+            yaxis: {
+              lines: {
+                show: false, //or just here to disable only y axis
+              },
+            },
+          },
+          responsive: [
+            {
+              breakpoint: 1360,
+              options: {
+                chart: {
+                  width: 350,
+                  // height: 350,
+                },
+              },
+            },
+            {
+              breakpoint: 1100,
+              options: {
+                chart: {
+                  width: 450,
+                  // height: 350,
+                },
+              },
+            },
+            {
+              breakpoint: 500,
+              options: {
+                chart: {
+                  width: 350,
+                  // height: 350,
+                },
+              },
+            },
+            {
+              breakpoint: 400,
+              options: {
+                chart: {
+                  width: 280,
+                  // height: 350,
+                },
+                plotOptions: {
+                  bar: {
+                    horizontal: false,
+                    distributed: true,
+                    borderRadius: 10,
+                    dataLabels: {
+                      show: false,
+                      position: 'top',
+                    },
+                  },
+                },
+              },
+            },
+          ],
+          plotOptions: {
+            bar: {
+              horizontal: true,
+              distributed: true,
+              borderRadius: 10,
+              dataLabels: {
+                position: 'top',
+              },
+            },
+            dataLabels: {
+              enabled: false,
+              style: {
+                colors: ['#333'],
+              },
+              offsetX: 20,
+            },
+          },
+
+          legend: {
+            show: false,
+            position: 'top',
+            horizontalAlign: 'left',
+            floating: true,
+            offsetY: -10,
+            offsetX: -5,
+          },
+          dataLabels: {
+            enabled: true,
+            textAnchor: 'center',
+            style: {
+              colors: ['#fff'],
+            },
+
+            formatter: function (val: any, opt: any) {
+              return opt.w.globals.labels[opt.dataPointIndex] + ':  ' + val;
+            },
+            offsetX: 0,
+          },
+          yaxis: {
+            show: false,
+            reversed: true,
+          },
+          tooltip: {
+            theme: 'dark',
+            x: {
+              show: false,
+            },
+            y: {
+              show: false,
+            },
+          },
+          xaxis: {
+            show: false,
+
+            labels: {
+              show: false,
+            },
+            categories: this.schoolWithZoneAndCurriculumFilter?.labels.map(
+              (label: any) => (this.currentLang == 'en' ? label.En : label.Ar)
+            ),
+          },
+        };
+      });
   }
+
+  // ngAfterViewInit(): void {
+  //   // Wait for ng-apexcharts to initialize
+  //   setTimeout(() => {
+  //     const apexChart: any = this.chart.chart;
+
+  //     if (apexChart && apexChart.w) {
+  //       const segments = apexChart.w.globals.dom.baseEl.querySelectorAll(
+  //         '.apexcharts-donut-series path'
+  //       );
+
+  //       segments.forEach((segment: any) => {
+  //         segment.setAttribute('rx', '10'); // Set border radius for x-axis
+  //         segment.setAttribute('ry', '10'); // Set border radius for y-axis
+  //       });
+  //     }
+  //   });
+  // }
 }
 
 // res = {
